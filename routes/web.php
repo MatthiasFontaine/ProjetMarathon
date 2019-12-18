01ccc8dca919ba26c895cc298040a7c7032788e6
MatthiasFontaine <58508344+MatthiasFontaine@users.noreply.github.com>
2019-12-18 13:54:33 +0100

    Add route laravel

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Genre;
+use App\Http\Resources\GenreResource;
+use App\Http\Resources\SerieResource;
+use App\Serie;
+
+Auth::routes();
+
+Route::get('/', 'MainController@index')->name('home');
+
+
+Route::get('/series/{series}', 'MainController@find');
+
+
+Route::get('/series/{series}/comments', 'MainController@comments');
