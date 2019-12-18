0c9682d0ca7d9284bef274945108fff2cf3a0886
LucieSimon <49950999+LucieSimon@users.noreply.github.com>
2019-12-19 00:02:40 +0100

    series (toutes, avec un grid !!!)

---
+++ series.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<htmllang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet"> 
+
+    <title>Watch it!</title>
+
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/screen.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="background">
+        <img src="/img/bg.png" id="background_image">
+        <div id="filtre"></div>
+        <div id="text">
+
+        <header class="header">
+
+            <p>Watch it!</p>
+
+                <nav>
+                    <div class="recherche">
+
+            
+
+                        <h1><a href="/resources/views/episode.blade.php">Explorer</a></h1>
+                        <h1><a href="episode.php">Critiques</a></h1>
+                        <h1><a href="episode.blade.php">Mes critiques</a></h1>
+                        <h1><a href="">Mon compte</a><h1>
+
+                        <form method="post">
+                                <input type="text" name="search">
+                                <input type="submit" alt="OK" value="OK">
+                        </form>
+
+                    </div>
+                </nav>
+
+        </header>
+
+
+        <div class="liste_series"> 
+
+        <div><img src="/img/bj.jpg">
+        <img src="/img/bj.jpg">
+        <img src="/img/bj.jpg"></div>
+        <div><img src="/img/bj.jpg">
+        <img src="/img/bj.jpg">
+        <img src="/img/bj.jpg"></div>
+        <div><img src="/img/bj.jpg">
+        <img src="/img/bj.jpg">
+        <img src="/img/bj.jpg"></div>
+        <div><img src="/img/bj.jpg">
+        <img src="/img/bj.jpg">
+        <img src="/img/bj.jpg"></div>
+        <div><img src="/img/bj.jpg">
+        <img src="/img/bj.jpg">
+        <img src="/img/bj.jpg"></div>
+        
+        </div>
+
+</body>
+</html>
