f8c7b1439429f16ae7096635a28ef89e1889b6ea
MatthiasFontaine <58508344+MatthiasFontaine@users.noreply.github.com>
2019-12-19 16:06:37 +0100

    Index final

--- index.blade.php
+++ index.blade.php
@@ -1,103 +1,104 @@
 <!DOCTYPE html>
-<htmllang="fr">
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
 
     <title>Watch it!</title>
 
-    <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/screen.css" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
 
-    <div class="background">
-        <img src="/img/bg.png" id="background_image">
-        <div id="filtre"></div>
-        <div id="text">
-
+<div class="background">
+    <img src="{{url("/img/bg.png")}}" id="background_image">
+    <div id="filtre"></div>
+    <div id="text">
         <header class="header">
 
-            <p>Watch it!</p>
+            <a href="/"><p>Watch it!</p></a>
 
-                <nav>
-                    <div class="recherche">
+            <nav>
+                <div class="recherche">
 
-            
 
-                        <h1><a href="/resources/views/episode.blade.php">Explorer</a></h1>
-                        <h1><a href="episode.php">Critiques</a></h1>
-                        <h1><a href="episode.blade.php">Mes critiques</a></h1>
-                        <h1><a href="">Mon compte</a><h1>
 
-                        <form method="post">
-                                <input type="text" name="search">
-                                <input type="submit" alt="OK" value="OK">
-                        </form>
+                    <h1><a href="/series">Explorer</a></h1>
+                    @if(Auth::user()!=null)
+                        <h1><a href="/user/{{Auth::user()->id}}">Voir compte</a></h1>
+                    @else
+                        <h1><a href="/login">Se connecter</a></h1>
+                    @endif
+                    <form method="post">
+                        <style>
+                            .recherche form{
+                                margin-top: 4rem;
+                            }
+                        </style>
+                        <input type="text" name="search">
+                        <input type="submit" alt="OK" value="OK">
+                    </form>
 
-                    </div>
-                </nav>
+                </div>
+            </nav>
 
         </header>
 
-        
+
         <p id="slogan">Watch it, and let's talk about it!</p>
 
-        <img src="/img/arrow.png" id="arrow">
+        <img src="https://popupfilmresidency.org/wp-content/uploads/2019/05/white-down-arrow-png-2.png" id="arrow">
 
-        
 
-    <div class="sliders">
 
-        <h1 id="slider_title">Nos dernières critiques</h1>
+        <div class="sliders">
 
-        <div class="first_slider"> 
+            <h1 id="slider_title">Nos dernières critiques</h1>
 
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">1ere critique</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">1ere critique</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">1ere critique</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">1ere critique</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">1ere critique</a></div></div>
+            <div class="first_slider">
+                @foreach($commentaireRecent as $com)
+                    <div><img src={{$com->urlImage}}><div class="slider_hover"><a href="/series/{{$com->id}}">Voir</a></div></div>
+                @endforeach
 
+            </div>
+
+            <h3><a href="/series">Tout voir</a></h3>
+
+
+                    <h1 id="slider_title">Les mieux notés</h1>
+
+                    <div class="second_slider">
+
+                        @foreach($serieRecente as $serie)
+                            <div><img src={{$serie->urlImage}}><div class="slider_hover"><a href="/series/{{$serie->id}}">Voir</a></div></div>
+                        @endforeach
+
+                    </div>
+
+                    <h3><a href="/series">Tout voir</a></h3>
+
+
+                            <h1 id="slider_title">Nos critiques vidéo</h1>
+
+                            <div class="third_slider">
+
+                                <div>
+                                    <div><a href="https://www.youtube.com/watch?v=zIVQlObaLIs&feature=youtu.be&fbclid=IwAR0IkQ6pxgVhj2XnWtaFaubqj_hAEDMZFJlQMEVPujPPnMtFpbzsG8QvZZ8"><img src="{{url("img/bojackart.png")}}" width="100%"> </a></div>
+                                </div>
+                                <div><a href="https://www.youtube.com/watch?v=zIVQlObaLIs&feature=youtu.be&fbclid=IwAR0IkQ6pxgVhj2XnWtaFaubqj_hAEDMZFJlQMEVPujPPnMtFpbzsG8QvZZ8"><img src="{{url("img/thecrowart-02.png")}}" width="100%"> </a></div>
+
+                            </div>
         </div>
 
-        <h3><a href"">Tout voir</a><h3>
 
-
-        <h1 id="slider_title">Séries</h1>
-
-        <div class="second_slider">
-
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">Voir</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">Voir</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">Voir</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">Voir</a></div></div>
-                <div><img src="/img/bj.jpg"><div class="slider_hover"><a href="">Voir</a></div></div>
-
-        </div>
-
-        <h3><a href"">Tout voir</a><h3>
-
-
-        <h1 id="slider_title">Nos critiques vidéo</h1>
-
-        <div class="third_slider"> 
-
-                <div><a href=""><img src=""></div>
-                <div><a href=""><img src=""></div>
-
-        </div>
-    </div>
-
-        
 
     </div>
-    </div>
+</div>
 </body>
 
 </html>
